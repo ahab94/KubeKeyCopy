766a7e378a4b696d685bdeb074dbb7b1ce27f3f0
ahab94 <abdshah94@gmail.com>
2018-06-10 01:25:28 +0500

    cover corner cases

--- AddMyKeyPlease.go
+++ AddMyKeyPlease.go
@@ -3,14 +3,17 @@ package main
 import (
 	"os"
 	"flag"
+	"log"
 )
 
 func main() {
 	publicKey := flag.String("key", "", "Public key to be copied (required)")
 	username := flag.String("username", "root", "Username (default: root")
+
 	flag.Parse()
 
 	if *publicKey == "" {
+		log.Println("Usage: ")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -19,6 +22,12 @@ func main() {
 
 	if *username != "root" {
 		sshFile = "/home/" + *username + "/.ssh/authorized_keys"
+	}
+
+	if _, err := os.Stat(sshFile); os.IsNotExist(err) {
+		log.Println("error: No such file or directory: ", sshFile)
+		log.Println("Make sure the authorized_keys file exist")
+		os.Exit(2)
 	}
 
 	f, err := os.OpenFile(sshFile, os.O_APPEND|os.O_WRONLY, 0600)
